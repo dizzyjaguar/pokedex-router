d63f70236b63b7c1c0187df1ea5e0ee44ac4cabc
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-16 17:43:53 -0800

    router working for about and list page, but now i need to fix my api and render pokemon

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import React, { Component }from 'react';
+import { 
+  Route, 
+  Switch,
+  Link,
+  BrowserRouter as Router, 
+} from 'react-router-dom';
 import Header from './Header.js';
 import PokeList from './PokeList.js';
-import Pokeitem from './PokeItem.js'
+import About from './About.js';
+
 import request from 'superagent';
 import './App.css';
 
 
 export default class App extends Component {
-  state = { 
-    pokedex: [],
-    totalPokemon: 801,
-    perPage: 20,
-    totalPage: 41
-  }
-
-  async loadPokemon() {
-    const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
-    let queryString = window.location.hash.slice(1);
-    const url = `${URL}?${queryString}`;
-
-    const searchedPokemon = await request.get(url);
-    this.setState({ 
-      pokedex: searchedPokemon.body.results,
-      totalPokemon: searchedPokemon.body.count,
-    }) 
-      const newTotalPage = Math.ceil(this.state.totalPokemon/this.state.perPage);
-      this.setState({ totalPage: newTotalPage });
-  }
-
-  async componentDidMount() {
-    const data = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex/');
-    this.setState({ pokedex: data.body.results })
-    this.setState({ totalPokemon: data.body.count })
-  }
+  
 
   render() {
     return (
+      <Router>
       <div>
         <Header></Header>
-        <PokeList pokedex={this.state.pokedex} />
+        <Link to='/'>HomePage</Link>
+        <Link to='/about'>About</Link>
+        {/* <PokeList pokedex={this.state.pokedex} /> */}
+        <Switch>
+          <Route exact path='/about' component={About} />
+          <Route exact path ='/' component={PokeList} />
+        </Switch>
+
+
       </div>
+      </Router>
     )
   }
 }
--- src/PokeList.js
+++ src/PokeList.js
@@ -6,14 +6,14 @@ export default class PokeList extends Component {
     
     render() {
 
-        const pokeNodes = this.props.pokedex.map(poke => <PokeItem pokeItem={poke} />)
+        
 
         return (
             <div>
-
+        
                 <ul id='poke-list'>
                 
-                    {pokeNodes}
+                    PokeList
                             
                 </ul>
             </div>
